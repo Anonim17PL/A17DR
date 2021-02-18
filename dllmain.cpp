e725acaefa25901ea6f69a4d75d1594dbf555067
Anonim17PL <76495160+Anonim17PL@users.noreply.github.com>
2021-02-18 14:22:37 +0100

    Verison 1.1

---
+++ dllmain.cpp
@@ -0,0 +1,114 @@
+﻿// dllmain.cpp : Definiuje punkt wejścia dla aplikacji DLL.
+#include "pch.h"
+#include <fstream>
+#include <iostream>
+#include <ctime>
+#include <stdio.h>
+#include <math.h>
+#include <string>
+#include <sstream>
+using namespace std;
+
+int secsprev;
+unsigned short varindexmax;
+int secondprev=0;
+intptr_t val[999];
+unsigned short varindexprev = -1;
+intptr_t Sval[5];
+
+ofstream plik;
+string filename;
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
+extern "C" {
+
+	__declspec(dllexport)void __stdcall PluginStart(void* aOwner)
+	{
+		char bufor[64];
+		time_t czas;
+		time(&czas);
+		tm czasTM;
+		localtime_s(&czasTM, &czas);
+		strftime(bufor, sizeof(bufor), "%#d.%m.%y %H.%M", &czasTM);
+		filename = string(bufor) + string(".csv");
+
+	}
+
+
+	__declspec(dllexport)void __stdcall AccessVariable(unsigned short varindex, float* value, bool* write)
+	{
+		unsigned int seconds;
+		seconds = Sval[3];
+		val[varindex] = (intptr_t)*value;
+		plik.open(filename, ios::out | ios::app);
+
+		int hour = seconds / 3600;
+		int minute = (seconds % 3600) / 60;
+		int second = seconds % 60;
+
+		if (varindexmax < varindex) {
+			varindexmax = varindex;
+		}
+
+		/*if (varindex == 0) {
+			plik << endl << Sval[0] << "." << Sval[1] << "." << Sval[2] << "," << hour << ":" << minute << ":" << second << "," << Sval[4] << "," << val[varindex];
+		}
+		else {
+			plik << "," << val[varindex];
+		}*/
+		int i;
+		if ((seconds - secsprev) >= 1) {
+			secsprev = seconds;
+			unsigned short i = 0;
+			for (i = 0; i <= varindexmax; i++) {
+
+				if (i == 0) {
+					plik << endl << Sval[0] << "." << Sval[1] << "." << Sval[2] << "," << hour << ":" << minute << ":" << second << "," << Sval[4] << "," << val[i];
+				}
+				else {
+					plik << "," << val[i];
+				}
+			}
+		}
+		if ((seconds - secsprev) <= -1) {
+			secsprev = seconds;
+		}
+		plik.close();
+	}
+
+
+	__declspec(dllexport)void __stdcall AccessTrigger(unsigned short triggerindex, bool* active)
+	{
+
+	}
+
+	__declspec(dllexport)void __stdcall AccessSystemVariable(unsigned short varindex, float* value, bool* write)
+	{
+		Sval[varindex] = (intptr_t)*value;
+	}
+
+	__declspec(dllexport)void __stdcall AccessStringVariable(unsigned short varindex, char* value, bool* write)
+	{
+
+	}
+
+	__declspec(dllexport)void __stdcall PluginFinalize()
+	{
+
+	}
+}
